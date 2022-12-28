0b011bcca57a832e8fceb0d10ab615eaf9c78ab2
samvrny <samvrny@gmail.com>
2022-12-28 11:10:51 -0600

    attempting to add express-session and mongo-connect to the application. Will see if it works soon enough

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const path = require('path');
 const allRoutes = require('./controllers');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const db = require('./config/connection');
+
+//set up a session
+const sess = {
+    secret: 'Chicken Wing Chicken Dinner',
+    store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost:27017/mycookbook'
+    })
+}
 
 //import the database and set up the port
-const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 
 //set up express
 const app = express();
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
