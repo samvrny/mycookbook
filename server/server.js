a750bf2569edcb2d6105d3726260dbded5f0bf80
samvrny <samvrny@gmail.com>
2022-12-27 23:01:31 -0600

    set up initial server and config files. Need to figure out how to work Auth files in

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+//import the database and set up the port
+const db = require('./config/connection');
+const PORT = process.env.PORT || 3001;
+
+//set up express
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+//open the database and start the server
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Now listening to MyCookbooks server on PORT ${PORT}!`);
+    })
+});
