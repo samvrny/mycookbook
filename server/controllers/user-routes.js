f8b55b3426292e79d66c8014d2920d159ad93c9a
samvrny <samvrny@gmail.com>
2022-12-28 19:48:25 -0600

    all user routes completed

--- server/controllers/user-routes.js
+++ server/controllers/user-routes.js
@@ -11,16 +11,19 @@ router.get('/', (req, res) => {
     });
 });
 
-//TODO: Finish creating a session once the user is created for them. Upon completion of
-//the sign up, I want to user to be directed to their dashboard
-
 router.post('/', (req, res) => {
     User.create({
         username: req.body.username,
         password: req.body.password,
         email: req.body.email
     })
-    .then(userData => res.json(userData))
+    .then(userData => {
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.username = userData.username;
+            req.session.loggedIn = true;
+        })
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err)
@@ -51,12 +54,19 @@ router.post('/login', (req, res) => {
             req.session.username = userData.username;
             req.session.loggedIn = true;
 
-            res.json({ user: userData, message: ' 🍐🍐🍐 This worked! Log in completed. 🍉🍉🍉', session: req.session })
+            res.json({ user: userData, message: ' 🍌🍌🍌 This worked! Log in completed. 🍒🍒🍒', session: req.session })
         });
     });
 });
 
-//TODO: Create a logout route for the user to destroy the session and log them out.
-//Need to shorten the time of the session so I can tell if the session is being created properly
-//and deleted properly.
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        })
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
