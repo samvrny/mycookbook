27e3634f6ed2409fbddcd86c2a0b669e5411168b
samvrny <samvrny@gmail.com>
2022-12-30 15:22:03 -0600

    got the delete recipe route working correctly. Just need to finish the 'PUT' route, which is giving me an intense amount of trouble

--- server/controllers/recipe-routes.js
+++ server/controllers/recipe-routes.js
@@ -1,15 +1,12 @@
 const router = require('express').Router();
-const { User, Recipe } = require('../models');
+const { User } = require('../models');
 const auth = require('../utils/auth');
 
 router.get('/', (req, res) => {
     User.findOne({
         _id: req.session.user_id
     })
-        .then(userData => {
-            console.log(userData.savedRecipes, 'Hello World')
-            res.json(userData.savedRecipes)
-        })
+        .then(userData => res.json(userData.savedRecipes))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
@@ -29,56 +26,34 @@ router.post('/newrecipe', auth, (req, res) => {
         })
 })
 
-//TODO: May need to update the put route later, to make sure it isn't just updating the overall database, 
-//but the users recipes as well
 router.put('/updaterecipe/:id', auth, (req, res) => {
-
-    //ITS A .THEN AFTER FINDING THE USER!!!!!!!!!!!!!!
-    User.findByIdAndUpdate({
-        _id: req.session.user_id
-    })
+    User.findById(
+        { _id: req.session.user_id },
+    )
     .then(userData => {
-        userData.savedRecipes.id(req.params.id).name = req.body.name
-        userData.savedRecipes.id(req.params.id).ingredients = req.body.ingredients
-        userData.savedRecipes.id(req.params.id).instructions = req.body.instructions
-        res.json(userData.savedRecipes)
+        const user = userData
+        console.log(user)
+        user.save()
     })
-    // Recipe.findOneAndUpdate(
-    //     { _id: req.params.id },
-    //     {
-    //         name: req.body.name,
-    //         ingredients: req.body.ingredients,
-    //         instructions: req.body.instructions
-    //     },
-    //     { new: true, runValidators: true }
-    // )
-    //     .then(recipeData => {
-    //         if (!recipeData) {
-    //             res.status(404).json({ message: 'No recipe found with this id.' });
-    //             return;
-    //         }
-    //         res.json(recipeData)
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(400).json(err);
-    //     })
+        // .then(userData => {
+        //         userData.savedRecipes.id(req.params.id).name = req.body.name
+        //         userData.savedRecipes.id(req.params.id).ingredients = req.body.ingredients
+        //         userData.savedRecipes.id(req.params.id).instructions = req.body.instructions
+        //     res.json(userData.savedRecipes)
+        // })
 })
 
-// router.delete('/:id', auth, (req, res) => {
-//     Recipe.findOneAndDelete({
-//         _id: req.params.id
-//     })
-//         .then(({ _id }) => {
-//             return User.findByIdAndUpdate(
-//                 { _id: req.session.user_id },
-//                 { $pull: { savedRecipes: _id } },
-//                 { new: true }
-
-//             )
-//         })
-//         .then(recipeData => res.json(recipeData))
-//         .catch(err => res.send(err))
-// })
+router.delete('/deleterecipe/:id', auth, (req, res) => {
+    User.findOneAndUpdate(
+        { _id: req.session.user_id },
+        { $pull: { savedRecipes: { _id: req.params.id } } },
+        { new: true }
+    )
+        .then(userData => res.json(userData))
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
 
 module.exports = router;
