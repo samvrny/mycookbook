544b1ca65139a54c1029704731f44fd6db520fa0
samvrny <samvrny@gmail.com>
2023-01-04 22:26:34 -0600

    added font loader utility

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,13 +1,17 @@
 import React from "react";
-import auth from "../../utils/auth";
+// import auth from "../../utils/auth";
+import { Link } from 'react-router-dom';
+import loadWebFont from "../../utils/webfont";
 
 function Header() {
+    
+    loadWebFont(['Black Han Sans', 'Urbanist'])
 
     return(
         <header>
             <h1>My Cookbook</h1>
             <nav>
-                
+                <Link></Link>
             </nav>
         </header>
     )
