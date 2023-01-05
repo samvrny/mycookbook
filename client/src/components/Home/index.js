15047c2ce2981d8dd99a6e8273b5d6218b37f195
samvrny <samvrny@gmail.com>
2023-01-04 23:33:28 -0600

    working on website design

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,16 +1,21 @@
 import React from "react";
+import loadWebFont from "../../utils/webfont";
 
 function Home() {
+
+    loadWebFont(['Urbanist'])
+
     return (
-        <section className='Home'>
-            <h1>
-                Welcome to the home page! You Made it!
-            </h1>
-            <div>
-                <h2>Content</h2>
+        <section className='Home flex column align-center'>
+            <div className='welcome-head'>
+                <h1>
+                    Welcome to My Cookbook! You Made it!
+                </h1>
+            </div>
+            <div className='welcome flex column align-center justify-center'>
                 <p>
                     Welcome to My Cookbook! food fruit bread orange strawberry
-                    pancake muffin bacon. You need somewhere to write down recipes, 
+                    pancake muffin bacon. You need somewhere to write down recipes,
                     so here you go! This is a place for you to do so. This application
                     is free to use and easy to navigate. I hope you enjoy!
                 </p>
@@ -25,12 +30,12 @@ function Home() {
                 <h2>Search Recipes</h2>
                 <p>
                     Not wanting to write your own recipes, or have the time?
-                    Search for a recipe to use below from the food network, New York times, 
+                    Search for a recipe to use below from the food network, New York times,
                     and others. Search for a recipe below!
                 </p>
                 <label for='nyt'>Search New York Times:</label>
                 <input id='nyt' type='text'></input>
-                <br/>
+                <br />
                 <p>
                     Like the recipe you found? Create a free account here, and save the
                     link in your 'Links' section, and never lose great recipes you found again!
