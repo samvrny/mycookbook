771ce2108f1437d4a0ce12df033ecc45e4a92faf
samvrny <samvrny@gmail.com>
2023-01-04 22:08:46 -0600

    added a checkered background to header and made a webfont file

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,19 +6,8 @@ function Header() {
     return(
         <header>
             <h1>My Cookbook</h1>
-            <button onClick={auth}>AUTH CHECK</button>
             <nav>
-                <ul>
-                    {/* {auth() ? (
-                        <ul>
-                            Im Logged In!
-                        </ul>
-                    ) : (
-                        <ul>
-                            Im NOT Logged In!
-                        </ul>
-                    )} */}
-                </ul>
+                
             </nav>
         </header>
     )
