c76482f050f7d144687072a524e13f795a3d21e7
samvrny <samvrny@gmail.com>
2023-01-27 11:47:37 -0600

    wrote first get request for the group

---
+++ server/controllers/group-routes.js
@@ -0,0 +1,14 @@
+const router = require('express');
+const { User } = require('../models');
+const auth = require('../utils/auth');
+
+router.get('/', auth, (req, res) => {
+    User.findOne({
+        _id: req.session.user_id
+    })
+    .then(userData => res.json(userData.savedGroups))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
