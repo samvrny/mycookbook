bf604abfac66fb28d9d45c2eb2c37cdf18b44159
samvrny <samvrny@gmail.com>
2023-01-27 11:44:22 -0600

    updated the index files for the controllers and the models

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const recipeRoutes = require('./recipe-routes');
 const linkRoutes = require('./link-routes');
+const groupRoutes = require('./group-routes');
 
 router.use('/user', userRoutes);
 router.use('/recipe', recipeRoutes);
 router.use('/link', linkRoutes);
+router.use('/group', groupRoutes);
 
 //WHAT is this below? don't know have to look up
 router.use((req, res) => {
